15f1700034c679a5723d49ba60a899ad05d4f839
Albert <albertpencas@gmail.com>
2024-11-28 14:40:41 +0100

    Canvis App_Assistencia

--- API_AppAssistencia/db_assistencia.py
+++ API_AppAssistencia/db_assistencia.py
@@ -1,13 +1,24 @@
-from fastapi import Query
 from client import db_client
-from typing import Optional
+from mysql.connector import Error
 
-def fetch_all_usuaris():
+def fetch_usuari_by_id(id):
     try:
+        # Crear conexió a la base de dades
         conn = db_client()
-        cur = conn.cursor()
-    except Exception as e:
-        return {"status": -2, "message": f"Error de connexió:{e}" }
+        cur = conn.cursor(dictionary=True)  # Retorna los resultats com diccionaris
+
+        # Consulta per obtenir l'usuari per ID
+        query = "SELECT * FROM usuaris WHERE idUsuari = %s"
+        cur.execute(query, (id,)) 
+
+        # Obtenim el resultat
+        result = cur.fetchone()
+        return result
+
+    except Error as e:
+        print(f"Error en la base de dades: {e}")
+        return None
+
     finally:
-        conn.close()
-    return assistencia_list
+        if conn.is_connected():
+            conn.close()
--- API_AppAssistencia/main.py
+++ API_AppAssistencia/main.py
@@ -49,16 +49,12 @@ class tablaClasse(BaseModel):
 @app.get("/usuaris/{id}", response_model=tablaUsuari)
 def read_usuari_id(id: int):
     try:
-        # Llama a la función en db_assistencia para obtener datos del usuario
         usuari = db_assistencia.fetch_usuari_by_id(id)
 
         if usuari is None:
-            # Lanzar excepción si no se encuentra el usuario
             raise HTTPException(status_code=404, detail="Usuari no trobat")
 
-        # Retorna el usuario como un modelo Pydantic
         return tablaUsuari(**usuari)
 
     except Exception as e:
-        # Manejo genérico de errores
         raise HTTPException(status_code=500, detail=f"Error intern: {e}")
